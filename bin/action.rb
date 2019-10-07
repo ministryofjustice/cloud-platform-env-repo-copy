8cda69af72bfb2c7c575a0249871979ea5938f17
David Salgado <david@digitalronin.com>
2019-10-07 16:19:42 +0100

    output event json

--- bin/action.rb
+++ bin/action.rb
@@ -7,4 +7,8 @@ puts "Terraform is at..."
 puts `which terraform`
 
 event = JSON.parse(File.read(ENV['GITHUB_EVENT_PATH']))
-puts event.inspect
+# puts event.inspect
+puts File.read(ENV['GITHUB_EVENT_PATH'])
+
+__END__
+
