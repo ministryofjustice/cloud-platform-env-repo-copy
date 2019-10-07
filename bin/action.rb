6cb5b199042ea1f07fc8e652cd2b41f9876b7b5d
David Salgado <david@digitalronin.com>
2019-10-07 16:15:03 +0100

    Dump the github event

--- bin/action.rb
+++ bin/action.rb
@@ -1,5 +1,10 @@
 #!/usr/bin/env ruby
 
+require "json"
+
 puts "Hello from ruby"
 puts "Terraform is at..."
 puts `which terraform`
+
+event = JSON.parse(File.read(ENV['GITHUB_EVENT_PATH']))
+puts event.inspect
