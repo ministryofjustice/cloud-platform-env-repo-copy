94c5eaee8e5026165a6513b09eaa133c8ae36a6a
David Salgado <david@digitalronin.com>
2019-10-08 15:00:38 +0100

    Try to create a comment on a PR
    I haven't supplied any commit parameters. I'm hoping this results
    in a comment at the PR level, as you can do via the web UI.

--- bin/reject-pr-if-multiple-namespaces.rb
+++ bin/reject-pr-if-multiple-namespaces.rb
@@ -10,6 +10,23 @@ data = JSON.parse File.read(ENV['GITHUB_EVENT_PATH'])
 pr_number = data.dig("pull_request", "number")
 puts "pr_number: #{pr_number}"
 
-# client = Octokit::Client.new(access_token: ENV['GITHUB_TOKEN'])
+name = data.dig("repository", "name")
+owner = data.dig("repository", "owner", "login")
+repo = [name, owner].join("/")
+
+client = Octokit::Client.new(access_token: ENV['GITHUB_TOKEN'])
 # current_repo = Octokit::Repository.from_url(event["repository"]["html_url"])
 
+client.create_pull_request_comment(
+  repo,
+  pr_number,
+  "This is the body"
+)
+
+# @comment = @client.create_pull_request_comment \
+#   @test_repo,
+#   @pull.number,
+#   new_comment[:body],
+#   new_comment[:commit_id],
+#   new_comment[:path],
+#   new_comment[:position]
