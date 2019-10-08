d0acf66d7382b90c5653bf1f4c69424599cabd04
David Salgado <david@digitalronin.com>
2019-10-08 13:17:17 +0100

    Try to git commit something

--- bin/action.rb
+++ bin/action.rb
@@ -5,10 +5,16 @@ require "json"
 puts "Hello from ruby"
 puts "Terraform is at..."
 puts `which terraform`
+puts `which git`
 
-event = JSON.parse(File.read(ENV['GITHUB_EVENT_PATH']))
+# event = JSON.parse(File.read(ENV['GITHUB_EVENT_PATH']))
 # puts event.inspect
-puts File.read(ENV['GITHUB_EVENT_PATH'])
+# puts File.read(ENV['GITHUB_EVENT_PATH'])
+
+File.open("foobar", "w") { |f| f.puts("Hello from Ruby at #{Time.now}") }
+system("git add foobar")
+system("git commit -m Whatever")
+system("git push origin master")
 
 __END__
 
