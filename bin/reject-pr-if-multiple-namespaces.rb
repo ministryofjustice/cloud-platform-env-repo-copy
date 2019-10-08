2e398470b0a431ca6713e546a17173f18be63a4a
David Salgado <david@digitalronin.com>
2019-10-08 14:46:36 +0100

    output the PR number

--- bin/reject-pr-if-multiple-namespaces.rb
+++ bin/reject-pr-if-multiple-namespaces.rb
@@ -5,8 +5,11 @@ require "octokit"
 
 puts "hello from the PR rejecter"
 
-puts File.read(ENV['GITHUB_EVENT_PATH'])
+data = JSON.parse File.read(ENV['GITHUB_EVENT_PATH'])
 
-client = Octokit::Client.new(access_token: ENV['GITHUB_TOKEN'])
-current_repo = Octokit::Repository.from_url(event["repository"]["html_url"])
+pr_number = data.dig("pull_request", "number")
+puts "pr_number: #{pr_number}"
+
+# client = Octokit::Client.new(access_token: ENV['GITHUB_TOKEN'])
+# current_repo = Octokit::Repository.from_url(event["repository"]["html_url"])
 
