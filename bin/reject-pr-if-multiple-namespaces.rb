0bd3491a7c1b08d266eb77c966bc3a1b94b8e9f9
David Salgado <david@digitalronin.com>
2019-10-08 14:37:51 +0100

    dump the github event

--- bin/reject-pr-if-multiple-namespaces.rb
+++ bin/reject-pr-if-multiple-namespaces.rb
@@ -4,3 +4,9 @@ require "json"
 require "octokit"
 
 puts "hello from the PR rejecter"
+
+puts File.read(ENV['GITHUB_EVENT_PATH'])
+
+client = Octokit::Client.new(access_token: ENV['GITHUB_TOKEN'])
+current_repo = Octokit::Repository.from_url(event["repository"]["html_url"])
+
