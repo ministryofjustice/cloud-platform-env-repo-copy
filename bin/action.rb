5a7d1420e80c22f324d9016c070877de1bc1194d
David Salgado <david@digitalronin.com>
2019-10-08 14:22:54 +0100

    Output the github token
    ...so I can try to use it in a script to create a commit

--- bin/action.rb
+++ bin/action.rb
@@ -12,14 +12,17 @@ puts `which git`
 
 pp ENV
 
+token = ENV['GITHUB_TOKEN']
+puts "token: #{token}"
+
 event = JSON.parse(File.read(ENV['GITHUB_EVENT_PATH']))
 # puts event.inspect
 # puts File.read(ENV['GITHUB_EVENT_PATH'])
 
-client = Octokit::Client.new(access_token: ENV['GITHUB_TOKEN'])
-current_repo = Octokit::Repository.from_url(event["repository"]["html_url"])
-last_commit = client.commits(current_repo).last
-client.create_commit(current_repo, "My commit message", last_commit.commit.tree.sha, last_commit.sha)
+# client = Octokit::Client.new(access_token: ENV['GITHUB_TOKEN'])
+# current_repo = Octokit::Repository.from_url(event["repository"]["html_url"])
+# last_commit = client.commits(current_repo).last
+# client.create_commit(current_repo, "My commit message", last_commit.commit.tree.sha, last_commit.sha)
 
 # File.open("foobar", "w") { |f| f.puts("Hello from Ruby at #{Time.now}") }
 # system("git config --global user.email 'github-actions@digital.justice.gov.uk'")
