67ab5fffe4637d2b6e01706e47d044dceb7d9475
David Salgado <david@digitalronin.com>
2019-10-11 15:34:37 +0100

    Commit TF file

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/cccd-dev/resources/ecr.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/cccd-dev/resources/ecr.tf
@@ -1,27 +1,27 @@
 terraform {
-  backend "s3" {}
+backend "s3" {}
 }
 
 module "cccd_ecr_credentials" {
-  source    = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=3.4"
-  repo_name = "cccd"
-  team_name = "laa-get-paid"
+source    = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=3.4"
+repo_name = "cccd"
+team_name = "laa-get-paid"
 
-  providers = {
-    aws = "aws.london"
-  }
+providers = {
+aws = "aws.london"
+}
 }
 
 resource "kubernetes_secret" "cccd_ecr_credentials" {
-  metadata {
-    name      = "cccd-ecr-credentials-output"
-    namespace = "cccd-dev"
-  }
+metadata {
+name      = "cccd-ecr-credentials-output"
+namespace = "cccd-dev"
+}
 
-  data {
-    access_key_id     = "${module.cccd_ecr_credentials.access_key_id}"
-    secret_access_key = "${module.cccd_ecr_credentials.secret_access_key}"
-    repo_arn          = "${module.cccd_ecr_credentials.repo_arn}"
-    repo_url          = "${module.cccd_ecr_credentials.repo_url}"
-  }
+data {
+access_key_id     = "${module.cccd_ecr_credentials.access_key_id}"
+secret_access_key = "${module.cccd_ecr_credentials.secret_access_key}"
+repo_arn          = "${module.cccd_ecr_credentials.repo_arn}"
+repo_url          = "${module.cccd_ecr_credentials.repo_url}"
+}
 }
