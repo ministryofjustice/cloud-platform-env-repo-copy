ebaa13284c4d29ef843146aa05984425569d5fa6
David Salgado <david.salgado@digital.justice.gov.uk>
2019-10-15 10:09:16 +0100

    terraform

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/becca-test-app-dev/resources/ecr.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/becca-test-app-dev/resources/ecr.tf
@@ -1,7 +1,7 @@
-module "becca_test_app_ecr_credentials" {
-  source    = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=3.4"
-  repo_name = "becca-test-app"
-  team_name = "tactical-products"
+              module "becca_test_app_ecr_credentials" {
+                source    = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=3.4"
+                repo_name = "becca-test-app"
+                team_name = "tactical-products"
 
   providers = {
     aws = "aws.london"
